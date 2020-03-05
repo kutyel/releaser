37e7cf2bdaf2d37729998a44057744862362fcaa
Flavio Corpa <flavio.corpa@47deg.com>
2020-03-05 10:41:13 +0100

    Cleanup and run ormolu

--- releaser/Main.hs
+++ releaser/Main.hs
@@ -1,25 +1,22 @@
 module Main (main) where
 
 import Releaser.Primitives
-import System.IO (hSetBuffering, stdout, stderr, BufferMode(..))
+import System.IO (BufferMode (..), hSetBuffering, stderr, stdout)
 
 main :: IO ()
 main = do
   hSetBuffering stdout LineBuffering
   hSetBuffering stderr LineBuffering
-
   -- prepare release
   -- TODO: pass a list directories
   gitAssertEmptyStaging
   version <- cabalBumpVersion "."
   let release = "v" <> version
   changelogPrepare
-
   -- make release
   tarball <- cabalSdist "."
   gitCommit $ "Bump to " <> release
   gitTag release
-
   -- syncing github/hackage
   gitPush "origin"
   gitPushTags "origin"
--- src/Releaser/Primitives.hs
+++ src/Releaser/Primitives.hs
@@ -1,57 +1,57 @@
-module Releaser.Primitives (
-  -- cabal utilities
-    CabalInfo(..)
-  , cabalRead
-  , cabalWriteVersion
-  , cabalBumpVersion
-  , cabalSdist
-  , cabalUpload
-  -- git primitives
-  , gitCheckout
-  , gitGetTags
-  , gitTag
-  , gitCommit
-  , gitPush
-  , gitPushTags
-  , gitAssertEmptyStaging
-  -- utilities
-  , prompt
-  , abort
-  , logStep
-  , changelogPrepare
-  ) where
+module Releaser.Primitives
+  ( -- cabal utilities
+    CabalInfo (..),
+    cabalRead,
+    cabalWriteVersion,
+    cabalBumpVersion,
+    cabalSdist,
+    cabalUpload,
+    -- git primitives
+    gitCheckout,
+    gitGetTags,
+    gitTag,
+    gitCommit,
+    gitPush,
+    gitPushTags,
+    gitAssertEmptyStaging,
+    -- utilities
+    prompt,
+    abort,
+    logStep,
+    changelogPrepare,
+  )
+where
 
-import System.IO 
-import System.Process
-import System.Console.Pretty (Color(..), color)
-import System.Environment (lookupEnv)
-import System.Exit (ExitCode(..), exitFailure)
-import Text.Regex.TDFA
-import Text.Regex.TDFA.Text
 import Data.Functor (void)
 import Data.List (intercalate)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
-import Text.ParserCombinators.ReadP (ReadP, readP_to_S)
 import Data.Version (parseVersion)
 import Distribution.PackageDescription.Parsec
-import Distribution.Verbosity (silent)
-import Distribution.Types.PackageId (pkgVersion, pkgName)
-import Distribution.Types.PackageDescription (package)
-import Distribution.Types.GenericPackageDescription (packageDescription)
-import Distribution.Types.Version (versionNumbers, mkVersion')
 import Distribution.Simple.Utils (tryFindPackageDesc)
+import Distribution.Types.GenericPackageDescription (packageDescription)
+import Distribution.Types.PackageDescription (package)
+import Distribution.Types.PackageId (pkgName, pkgVersion)
 import Distribution.Types.PackageName (unPackageName)
+import Distribution.Types.Version (mkVersion', versionNumbers)
+import Distribution.Verbosity (silent)
+import System.Console.Pretty (Color (..), color)
+import System.Environment (lookupEnv)
+import System.Exit (ExitCode (..), exitFailure)
+import System.IO
+import System.Process
+import Text.ParserCombinators.ReadP (ReadP, readP_to_S)
+import Text.Regex.TDFA
+import Text.Regex.TDFA.Text
 
 logStep :: String -> IO ()
-logStep str = 
-  putStrLn $ color Green ">> " <> str
+logStep str = putStrLn $ color Green ">> " <> str
 
 prompt :: String -> IO String
 prompt str = do
   putStr $ color Blue ">> " <> str
   hFlush stdout
-  getLine  
+  getLine
 
 promptRetry :: String -> IO ()
 promptRetry str =
@@ -62,10 +62,11 @@ abort str = do
   putStrLnErr $ color Red ">> " <> str
   exitFailure
 
-data CabalInfo = CabalInfo 
-  { name :: String
-  , version :: String
-  }
+data CabalInfo
+  = CabalInfo
+      { name :: String,
+        version :: String
+      }
 
 -- | Given a folder, find a Cabal file and read the package version
 cabalRead :: FilePath -> IO CabalInfo
@@ -75,32 +76,32 @@ cabalRead dir = do
   genericPackageDescription <- readGenericPackageDescription silent cabalFile
   let pkgversion = pkgVersion $ package $ packageDescription genericPackageDescription
       pkgname = pkgName $ package $ packageDescription genericPackageDescription
-      cabalinfo = CabalInfo 
-        { version = intercalate "." $ show <$> versionNumbers pkgversion
-        , name = unPackageName pkgname
-        }
+      cabalinfo =
+        CabalInfo
+          { version = intercalate "." $ show <$> versionNumbers pkgversion,
+            name = unPackageName pkgname
+          }
   logStep $ "Found " <> name cabalinfo <> "-" <> version cabalinfo
   return cabalinfo
 
 -- | Given a folder, find a Cabal file and update the package version
 cabalWriteVersion :: FilePath -> String -> IO ()
-cabalWriteVersion dir versionStr = do
+cabalWriteVersion dir versionStr =
   if validCabalVersion versionStr
-  then do
-    cabalFile <- tryFindPackageDesc dir
-    cabalinfo <- cabalRead dir
-    cabal <- T.readFile cabalFile
-    let versionPrev :: T.Text
-        versionPrev = cabal =~ ("version:[ \t]*" ++ version cabalinfo)
-    if versionPrev == ""
-    then abort $ "Failed to replace version in " <> cabalFile <> ", please open an issue at https://github.com/domenkozar/releaser/issues"
+    then do
+      cabalFile <- tryFindPackageDesc dir
+      cabalinfo <- cabalRead dir
+      cabal <- T.readFile cabalFile
+      let versionPrev :: T.Text
+          versionPrev = cabal =~ ("version:[ \t]*" ++ version cabalinfo)
+      if versionPrev == ""
+        then abort $ "Failed to replace version in " <> cabalFile <> ", please open an issue at https://github.com/domenkozar/releaser/issues"
+        else do
+          T.writeFile cabalFile $ T.replace versionPrev ("version: " <> T.pack versionStr) cabal
+          logStep $ "Bumped " <> name cabalinfo <> " to " <> versionStr
     else do
-      T.writeFile cabalFile $ T.replace versionPrev ("version: " <> T.pack versionStr) cabal
-      logStep $ "Bumped " <> name cabalinfo <> " to " <> versionStr
-  else do
-    promptRetry "Cabal version does not match /^[0-9]+([.][0-9]+)*$/"
-    void $ cabalBumpVersion dir
-    
+      promptRetry "Cabal version does not match /^[0-9]+([.][0-9]+)*$/"
+      void $ cabalBumpVersion dir
 
 validCabalVersion :: String -> Bool
 validCabalVersion version =
@@ -132,17 +133,16 @@ cabalUpload sdistTarball = do
   interactiveProcess (proc "cabal" ["upload", "--publish", sdistTarball]) $ \_ -> do
     promptRetry "cabal upload"
     cabalUpload sdistTarball
-    
+
 gitGetTags :: IO [String]
-gitGetTags = do
-  lines <$> readProcess "git" ["tag"] mempty
+gitGetTags = lines <$> readProcess "git" ["tag"] mempty
 
 -- TODO: what can we do if previous release process terminated and branch exists?
 gitCheckout :: String -> IO ()
 gitCheckout tag = do
   logStep $ "Running $ git checkout -b " <> tag
   -- TODO: check for existing branch
-  interactiveProcess (proc "git" ["checkout", "-b", tag]) $ \i -> do 
+  interactiveProcess (proc "git" ["checkout", "-b", tag]) $ \i -> do
     promptRetry "git checkout failed"
     gitCheckout tag
 
@@ -150,24 +150,23 @@ gitTag :: String -> IO ()
 gitTag tag = do
   logStep $ "Running $ git tag --annotate --sign " <> tag
   tags <- gitGetTags
-  if elem tag tags
-  then abort "git tag already exists, please delete it and start over"
-  else interactiveProcess (proc "git" ["tag", "--annotate", "--sign", tag]) $ \i -> do 
-    promptRetry "git tag failed"
-    gitTag tag
+  if tag `elem` tags
+    then abort "git tag already exists, please delete it and start over"
+    else interactiveProcess (proc "git" ["tag", "--annotate", "--sign", tag]) $ \i -> do
+      promptRetry "git tag failed"
+      gitTag tag
 
 gitCommit :: String -> IO ()
 gitCommit message = do
-  logStep $ "Running $ git commit "
-  interactiveProcess (proc "git" ["commit", "-a", "-m", message]) $ \i -> do 
+  logStep "Running $ git commit "
+  interactiveProcess (proc "git" ["commit", "-a", "-m", message]) $ \i -> do
     promptRetry "git commit failed"
     gitCommit message
-
 
 gitPush :: String -> IO ()
 gitPush remote = do
   logStep $ "Pushing git to " <> remote
-  interactiveProcess (proc "git" ["push", remote, "HEAD"]) $ \i -> do 
+  interactiveProcess (proc "git" ["push", remote, "HEAD"]) $ \i -> do
     promptRetry "git push"
     gitPush remote
 
@@ -181,8 +180,8 @@ gitAssertEmptyStaging = do
   logStep "Assserting there are no uncommitted files"
   output <- readProcess "git" ["status", "--untracked-files=no", "--porcelain"] mempty
   if output == ""
-  then return ()
-  else abort "git status is not clean"
+    then return ()
+    else abort "git status is not clean"
 
 changelogPrepare :: IO ()
 changelogPrepare = do
@@ -190,7 +189,7 @@ changelogPrepare = do
   editorEnv <- lookupEnv "EDITOR"
   case editorEnv of
     Nothing -> abort "please make sure $EDITOR is set"
-    Just editor -> do
+    Just editor ->
       -- TODO: prepare the changelog
       interactiveProcess (proc editor ["CHANGELOG.md"]) $ \i -> do
         logStep $ editor <> " failed with " <> show i <> ", retrying"
